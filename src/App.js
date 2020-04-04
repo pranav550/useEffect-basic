6b6e3d4e2f6854e19903deea96643b1d87a76a99
pranav550 <pranavverma550@gmail.com>
2020-04-04 19:04:11 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IntervalHookCounter from './components/IntervalHookCounter';
+//import HookCounterOne from './components/HookCounterOne';
+// import HookMouse from './components/HookMouse';
+//import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <HookCounterOne /> */}
+      {/* <HookMouse /> */}
+      {/* <MouseContainer /> */}
+      <IntervalHookCounter />
     </div>
   );
 }
